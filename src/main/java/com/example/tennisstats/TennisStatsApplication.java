df44f849d0982522b62558a3106e7a41c10167ff
Reda <redad@ext-theodo.fr>
2023-05-11 07:58:36 +0200

    [OPE] init spring boot project

---
+++ src/main/java/com/example/tennisstats/TennisStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tennisstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisStatsApplication.class, args);
+    }
+
+}
